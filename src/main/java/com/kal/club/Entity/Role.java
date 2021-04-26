2bbe6523b7ff4bb00c64a90069756f3ffc3484c6
kalvinzhao <kalvinzhao11@gmail.com>
2021-04-25 21:45:38 -0400

    initial commit

---
+++ src/main/java/com/kal/club/Entity/Role.java
@@ -0,0 +1,9 @@
+package com.kal.club.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+}
---
+++ src/main/java/com/kal/club/Entity/User.java
@@ -0,0 +1,11 @@
+package com.kal.club.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+
+}
