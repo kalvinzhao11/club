114e323495a9d02adf682b31907715f122ff7417
kalvinzhao <kalvinzhao11@gmail.com>
2021-04-26 12:39:09 -0400

    implemented exception handling

--- src/main/java/com/kal/club/Exception/ResourceNotFoundException.java
+++ src/main/java/com/kal/club/Exception/ResourceNotFoundException.java
@@ -1,5 +1,7 @@
 package com.kal.club.Exception;
 
-public class ResourceNotFoundException {
-
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(String message) {
+        super("RNFE: " + message);
+    }
 }
